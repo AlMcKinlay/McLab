b9320f8ca90cbfc1749a3d56615d0420c39ca1f7
Al McKinlay <github@10people.co.uk>
2026-01-28 23:26:15 +0000

    fix: lint issues

--- switch/src/index.js
+++ switch/src/index.js
@@ -1,6 +1,4 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
