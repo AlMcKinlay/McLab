b5f10c18c59df6b76bbbb6a6f7a47982c7e94a01
Al McKinlay <github@10people.co.uk>
2026-02-22 17:53:13 +0000

    feat(pokemon-list): update max pokemon

--- pokemon-list/src/app/api/getPokemon.js
+++ pokemon-list/src/app/api/getPokemon.js
@@ -1,5 +1,5 @@
 export const getAllPokemon = () => {
-	return fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=898").then(
-		(res) => res.json()
+	return fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1025").then(
+		(res) => res.json(),
 	);
 };
